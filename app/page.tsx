aa6fa4592626979a119b9c732f99e05ad9cf4de1
a-shahrabi <ardavan.shahrabi@dal.ca>
2025-02-19 22:23:14 -0500

    commit again

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function Home() {
+  return (
+   
+     <div className = "min-h-screen bg-white dark:bg-black text-black dark:text-white transition-colors duration-300">
+      <div className="mx-auto"></div>
+     </div>
+  );
+}
