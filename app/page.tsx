35996977051536f1274d7f13eea5d75029674736
a-shahrabi <ardavan.shahrabi@dal.ca>
2025-03-01 20:22:37 -0500

    experience is done

--- app/page.tsx
+++ app/page.tsx
@@ -210,7 +210,7 @@ export default function Home() {
   </motion.section>
           </section>
           <motion.section 
-          //
+          id="experience"
           initial = {{opacity: 0, y: 100}}
           animate = {{opacity: 1, y: 0}}
           transition = {{duration: 0.8}}
