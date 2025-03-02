e88574cf505a425fe6d9de8d9ad31c54356ee100
a-shahrabi <ardavan.shahrabi@dal.ca>
2025-03-01 20:21:11 -0500

    skills & Tech section

--- app/page.tsx
+++ app/page.tsx
@@ -138,7 +138,7 @@ export default function Home() {
           </motion.section>
 
           <motion.section
-          // 
+          id="skills"
         initial={{opacity: 0, y: 100}}
         animate={{opacity: 1, y: 0}}
         transition={{duration: 0.8}}
